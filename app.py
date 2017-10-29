de5fe07cb8106e84d85c88abd35afc4ab706a6a0
Raja Simon <rajasimon@icloud.com>
2017-10-29 23:36:22 +0530

    Chalice based application

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from chalice import Chalice
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+app = Chalice(app_name='fiobot')
+app.debug = True
+
+chatbot = ChatBot(
+    'fiobot',
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    database_uri=os.environ.get('DATABASE_URI')
+)
+
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+chatbot.set_trainer(ListTrainer)
+chatbot.train(conversation)
+
+
+@app.route('/')
+def index():
+    text = app.current_request.query_params.get('text')
+    response = chatbot.get_response(text)
+    return {'reply': response.text}
